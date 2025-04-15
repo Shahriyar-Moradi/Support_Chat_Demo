e9f9dba511428995cb9f6b6e9223e32924f22d6f
Shahriar <shahriar@Shahriars-MacBook-Air.local>
2025-04-15 13:43:13 +0400

    update embbeded

--- stream1-faqv1.1.py
+++ stream1-faqv1.1.py
@@ -44,8 +44,8 @@ while not pc.describe_index(INDEX_NAME).status.get('ready', False):
 index = pc.Index(INDEX_NAME)
 
 # Load fine-tuned embedding model
-ft_model = SentenceTransformer("/Users/shahriar/Desktop/Work/AbanTether/AbanTether/raggpt/pincone_RAG/embedded_fintune/distobert-finetuned-embedding-faq1-v1-1")
-# ft_model = SentenceTransformer("Shahriardev/distobert-finetuned-embedding-faq1-v1-1")
+# ft_model = SentenceTransformer("/Users/shahriar/Desktop/Work/AbanTether/AbanTether/raggpt/pincone_RAG/embedded_fintune/distobert-finetuned-embedding-faq1-v1-1")
+ft_model = SentenceTransformer("Shahriardev/distobert-finetuned-embedding-faq1-v1-1")
 print("Fine-tuned model dimension:", ft_model.get_sentence_embedding_dimension())
 
 # Currency mappings and keywords
